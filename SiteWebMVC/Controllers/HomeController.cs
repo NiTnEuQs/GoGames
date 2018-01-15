35109634c1d310e356c169cdf8bfa12f5d5b903b
nitneuqs <nitneuqtuod@hotmail.fr>
2018-01-15 14:22:12 +0100

    First commit

---
+++ SiteWebMVC/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SiteWebMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+    }
+}
